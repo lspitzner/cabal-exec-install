8465f5f56c00794fb1f1250bd873a3b4401f21bc
Lennart Spitzner <lsp@informatik.uni-kiel.de>
2015-01-06 08:30:24 +0100

    commentify pandoc (too large to be included by default)

--- cabal-exec-install.sh
+++ cabal-exec-install.sh
@@ -123,7 +123,7 @@ echo-install pointful
 echo-install pointfree
 # threadscope does not compile atm, without specifying constraint gtk >= 0.12.1 && < 0.13.
 # echo-install threadscope
-echo-install pandoc
+# echo-install pandoc
 echo-install alex "(required by gtk2hs-buildtools)"
 
 # echo-install gtk2hs-buildtools "(required by gtk)"
